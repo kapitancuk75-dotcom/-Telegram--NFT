569cc7e699d14b91c89cffe13f4408c47094becb
kapitancuk75-dotcom <kapitancuk75@gmail.com>
2026-03-14 03:16:26 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = int(os.getenv("CHAT_ID"))
+
+MAX_PRICE = float(os.getenv("MAX_PRICE", "5"))
