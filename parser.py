5ffb1aa46083bd6199cf34cb1f3ad48196650c69
kapitancuk75-dotcom <kapitancuk75@gmail.com>
2026-03-14 03:17:28 +0500

    Create parser.py

---
+++ parser.py
@@ -0,0 +1,24 @@
+import re
+
+def parse_nft(text):
+
+    name = None
+    price = None
+    url = None
+
+    name_match = re.search(r'([A-Za-z ]+ #\d+)', text)
+
+    if name_match:
+        name = name_match.group(1)
+
+    price_match = re.search(r'(\d+(\.\d+)?)\s?TON', text)
+
+    if price_match:
+        price = float(price_match.group(1))
+
+    url_match = re.search(r'(https://t.me/\S+)', text)
+
+    if url_match:
+        url = url_match.group(1)
+
+    return name, price, url
